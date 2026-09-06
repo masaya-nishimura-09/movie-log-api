75fcb142dec1a3170440c8a3c46aa55b6f505893
masaya <masaya.nishimura0927@gmail.com>
2026-09-06 13:44:28 +0900

    fix: movie domain

--- internal/domain/movie/movie.go
+++ internal/domain/movie/movie.go
@@ -1,24 +1,23 @@
 package movie
 
 type Movie struct {
-	ID              ID
-	Title           Title
-	OriginalTitle   Title
-	Overview        Overview
-	Genres          []Genre
-	PosterURL       PosterURL
-	ReleaseYear     ReleaseYear
-	Runtime         Runtime
-	Language        Language
-	OriginCountries []Country
+	ID               ID
+	Title            Title
+	OriginalTitle    Title
+	Overview         Overview
+	Genres           []Genre
+	PosterURL        PosterURL
+	ReleaseYear      *ReleaseYear
+	Runtime          Runtime
+	OriginalLanguage OriginalLanguage
+	OriginCountry    []Country
 }
 
 type ID uint
 type Overview string
-type Genre string
 type PosterURL string
 type ReleaseYear uint
 type Runtime uint
-type Language string
+type OriginalLanguage string
 type DisplayLanguage string
 type Country string
