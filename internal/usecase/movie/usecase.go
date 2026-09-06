19260574903f7ec7c2681412230622d5ca288424
masaya <masaya.nishimura0927@gmail.com>
2026-09-06 18:39:03 +0900

    add: movie usecase

---
+++ internal/usecase/movie/usecase.go
@@ -0,0 +1,61 @@
+package movie
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/masaya-nishimura-09/movie-log-api/internal/domain/movie"
+)
+
+type Usecase interface {
+	GetByID(
+		ctx context.Context,
+		movieID movie.ID,
+		language movie.DisplayLanguage,
+	) (*movie.Movie, error)
+	SearchByTitle(
+		ctx context.Context,
+		title movie.Title,
+		page movie.Page,
+		language movie.DisplayLanguage,
+	) (*movie.SearchResult, error)
+}
+
+type MovieUsecase struct {
+	movieService movie.MovieService
+}
+
+func NewMovieUsecase(
+	movieService movie.MovieService,
+) *MovieUsecase {
+	return &MovieUsecase{
+		movieService: movieService,
+	}
+}
+
+func (mu *MovieUsecase) GetByID(
+	ctx context.Context,
+	movieID movie.ID,
+	language movie.DisplayLanguage,
+) (*movie.Movie, error) {
+	m, err := mu.movieService.GetByID(ctx, movieID, language)
+	if err != nil {
+		return nil, fmt.Errorf("get movie by id: %w", err)
+	}
+
+	return m, nil
+}
+
+func (mu *MovieUsecase) SearchByTitle(
+	ctx context.Context,
+	title movie.Title,
+	page movie.Page,
+	language movie.DisplayLanguage,
+) (*movie.SearchResult, error) {
+	sr, err := mu.movieService.SearchByTitle(ctx, title, page, language)
+	if err != nil {
+		return nil, fmt.Errorf("search movies by title: %w", err)
+	}
+
+	return sr, nil
+}
